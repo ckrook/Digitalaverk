e53141b8f6a0970291a638edbae8297243a85d89
Charles Krook <charles.krook@gmail.com>
2022-03-31 20:29:31 +0200

    update style

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -12,9 +12,9 @@ function NFTDropPage() {
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* Left */}
-      <div className="bg-gradient-to-br from-cyan-200 to-rose-500 lg:col-span-4">
-        <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
-          <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2">
+      <div className="bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 lg:col-span-4">
+        <div className="my-8 flex flex-col items-center justify-center py-2 lg:min-h-screen">
+          <div className="rounded-2xl bg-gradient-to-r from-rose-100 to-teal-100 p-4">
             <img
               className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
               src="https://links.papareact.com/8sg"
@@ -46,14 +46,17 @@ function NFTDropPage() {
           </h1>
           <button
             onClick={() => (address ? disconnect() : connectWithMetamask())}
-            className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base"
+            className="rounded-full bg-rose-500 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base"
           >
-            Sign in
+            {address ? 'Sign out' : 'Sign in'}
           </button>
         </header>
         <hr className="my-2 border" />
         {address && (
-          <p>Your logged in with wallet {address.substring(0, 5)}...</p>
+          <p className="text-right text-sm text-gray-500">
+            Your logged in with wallet {address.substring(0, 5)}...
+            {address.substring(address.length - 5)}
+          </p>
         )}
         {/* Content */}
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
@@ -62,13 +65,13 @@ function NFTDropPage() {
             src="https://links.papareact.com/bdy"
             alt=""
           />
-          <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
+          <h1 className="text-3xl font-bold text-gray-800 lg:text-5xl lg:font-extrabold">
             The Antisocial Coding Club | NFT
           </h1>
-          <p className="pt-2 text-xl text-green-500">13 / 21 NFTs claimed</p>
+          <p className="pt-2 text-lg text-green-500">13 / 21 NFTs claimed</p>
         </div>
         {/* Mint Button */}
-        <button className="h-16 w-full rounded-full bg-red-500 font-bold text-white">
+        <button className="h-16 w-full rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 font-bold text-white">
           Mint NFT (0.01ETH)
         </button>
       </div>
