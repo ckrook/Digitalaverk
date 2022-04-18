723ac977c461378df5ed7eb8d5c2f7aa3961723a
Charles Krook <charles.krook@gmail.com>
2022-04-18 13:35:19 +0200

    commit

--- components/header/Header.js
+++ components/header/Header.js
@@ -5,6 +5,10 @@ import MainNav from './MainNav'
 function Header() {
   return (
     <header>
+      <div className="w-full bg-red-500 text-center text-sm font-semibold text-white">
+        This is only a conceptual website. All the collections are currently
+        launched on Rinkeby testnet.
+      </div>
       <TopNav />
       <MainNav />
     </header>
